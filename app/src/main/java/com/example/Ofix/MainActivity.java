6b12673dc6ce9e28471ae173fc3a3f7dd80ea936
Cruzato <gabrielcruzato@gmail.com>
2021-05-14 21:32:47 -0300

    f

--- app/src/main/java/com/example/Ofix/MainActivity.java
+++ app/src/main/java/com/example/Ofix/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.Ofix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     MyAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
